42154e800acdad39d3e6cf429e4d4d6e8df2a7f5
buseliiik <b.yilmaaazzz@hotmail.com>
2023-04-19 22:42:00 +0000

    s

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/buseliiik/REP02.git
+
+go 1.18
+
+require github.com/buseliiik/is105sem03 v0.0.0-20230419003157-b0267bd3cbe2
--- tcp_client.go
+++ tcp_client.go
@@ -1,28 +1,38 @@
 package main
 
 import (
-	"net"
+	"github.com/buseliiik/is105sem03/mycrypt"
 	"log"
+	"net"
 	"os"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:")
+	conn, err := net.Dial("tcp", "172.17.0.3:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
-    
+
 	log.Println("os.Args[1] = ", os.Args[1])
 
- 	_, err = conn.Write([]byte(os.Args[1]))
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03))
+	log.Println("Kryptert melding: ", string(kryptertMelding))
+
+	_, err = conn.Write([]byte(string(kryptertMelding)))
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	buf := make([]byte, 1024)
 	n, err := conn.Read(buf)
 	if err != nil {
 		log.Fatal(err)
-	} 
-	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
+	}
+
+	dekryptertMelding := mycrypt.DeKrypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03))
+	log.Println("Dekrypter melding: ", string(dekryptertMelding))
+
+	response := string(dekryptertMelding)
+	log.Printf("Reply from proxy: %s", response)
+
 }
